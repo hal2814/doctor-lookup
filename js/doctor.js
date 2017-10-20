8ca630f95e543972ecd8f17e1fb65175a3ebc3be
Nate McGregor <natecmcgregor@gmail.com>
2017-10-20 11:52:36 -0700

    create output loop in api function

--- js/doctor.js
+++ js/doctor.js
@@ -23,7 +23,16 @@ export class Doctor {
   callApi(promise){
     promise.then(function(response) {
       let body = JSON.parse(response);
-
+       for(let i = 0; i < body.length; ++i){
+         $('#picture').append("<img src='" + body.profile.image_url + "'>");
+         $('#name').append("<h3>Doctor name: " + body.practics.name + "</h3>");
+         $('#city').append("<h4>City: " + body.practics.visit_address.city + "</h4>");
+         $('#state').append("<h4>State: " + body.practics.visit_address.state + "</h4>");
+         $('#street').append("<h4>Street: " + body.practics.visit_address.street + "</h4>");
+         $('#zip').append("<h4>Zip: " + body.practics.visit_address.zip + "</h4>");
+         $('#phone').append("<h4>Phone: " + body.phones.number + "</h4>");
+         $('#bio').append("<h4>Bio: " + body.bio + "</h4>");
+       }
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
